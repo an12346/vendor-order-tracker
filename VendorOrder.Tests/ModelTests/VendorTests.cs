ecf9277f5b6c0ef04b38b61f4ac9f2f2c3b11f46
Anna Pittman <annaepittman@gmail.com>
2021-12-17 11:24:24 -0800

    create passing test for getting the name of a vendor

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -14,5 +14,16 @@ namespace VendorOrder.Tests
       Vendor newVendor = new Vendor("Susie's Bakery", "Bakery");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    [TestMethod]
+    public void GetNameOfVendor_ReturnsNameOfVendor_String()
+    {
+      string name = "Susie's Bakery";
+      Vendor newVendor = new Vendor(name, "Bakery");
+      string output = newVendor.Name;
+      Assert.AreEqual(name, output);
+    }
+
+    
   }
 }
